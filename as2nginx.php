25efd71fb4582fdd49fb53d9d1fac7cc1b601506
Alexander Bogdanov <syn@li.ru>
2013-10-07 01:28:16 +0400

    Using syslog

--- as2nginx.php
+++ as2nginx.php
@@ -1,6 +1,5 @@
 #!/usr/bin/env php
 <?php
-// console.php
 require_once __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Console as Console;
@@ -8,4 +7,17 @@ use Symfony\Component\Console as Console;
 $application = new Console\Application('as2nginx', '1.0.0');
 $application->add(new Theon\Nginx\Acl('acl'));
 $application->add(new Theon\Nginx\Geoswitch('geoswitch'));
-$application->run();
+$application->setCatchExceptions(false);
+try
+{
+    $application->run();
+}
+catch (Exception $e)
+{
+    openlog('as2nginx', LOG_NDELAY | LOG_PID | LOG_PERROR, LOG_USER);
+    syslog(LOG_ERR, sprintf('%s [ %s ]: %s',
+        get_class($e),
+        $e->getCode(),
+        strip_tags($e->getMessage())
+    ));
+}
